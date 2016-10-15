1cb428bb36a31d2a4c7e0ebda713c03ad6c5f184
jangmyounhoon <jsmk2009@naver.com>
2016-10-15 15:12:58 +0900

    da1 소스

---
+++ Day1/turtle2.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.forward(1)
+
+turtle.circle(100)
+turtle.right(90)
+turtle.circle(100)
+turtle.right(90)
+turtle.circle(100)
+turtle.right(90)
+turtle.circle(100)
+
+
+turtle.circle(50)
+turtle.right(90)
+turtle.circle(150)
+turtle.right(90)
+turtle.circle(200)
+turtle.right(90)
+turtle.circle(200)
+turtle.right(90)
+turtle.circle(200)             
